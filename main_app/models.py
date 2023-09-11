d6b1ebbdf22b8fcffd8d5a09c7c88ab44ab6699d
Sheikh Rokon Uzzman <mdorokon.u545@gmail.com>
2023-09-11 10:57:42 +0600

    Institution_info model create

--- main_app/models.py
+++ main_app/models.py
@@ -1,17 +1,18 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
 class Institution_info(models.Model):
     banners_image = models.ImageField(upload_to='banner_image')
-    news = models.CharField(max_length=1500)
     principal_name = models.CharField(max_length=200)
     vice_principal_name = models.CharField(max_length=200)
     principal_image = models.ImageField(upload_to='principal_image')
     vice_principal_image = models.ImageField(upload_to='vice_principal_image')
-    principal_message = models.CharField(max_length=1500)
-    vice_principal_message = models.CharField(max_length=1500)
-    institution_history = models.CharField(max_length=2000)
     institution_image = models.ImageField(upload_to='institution_image')
+    news = RichTextUploadingField()
+    principal_message = RichTextUploadingField()
+    vice_principal_message = RichTextUploadingField()
+    institution_history = RichTextUploadingField()
     
     class Meta:
         verbose_name = 'Institution_info'
