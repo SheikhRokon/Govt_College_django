727172604b764272a53491a61cc5413cb0bbf1a8
Sheikh Rokon Uzzman <mdorokon.u545@gmail.com>
2023-09-12 15:06:50 +0600

    up date

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,7 @@ from .views import *
 
 urlpatterns = [
      path('',index ,name='index'),
-     path('classRoutine',classRoutine ,name='classRoutine'),
+     path('classRoutine/',classRoutine ,name='classRoutine'),
+     path('notices/',notices ,name='notices'),
+     path('managing_committee/',managing_committee ,name='managing_committee'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Institution_info
+from .models import *
 
 # Create your views here.
 
@@ -14,3 +14,19 @@ def index(request):
 
 def classRoutine(request):
     return render(request, 'main_app/classRoutine.html')
+
+def notices(request):
+    notice = Notice.objects.all().order_by("-id")
+    
+    context = {
+        'notice': notice
+        }
+    return render(request, 'main_app/notices.html',context)
+
+def managing_committee(request):
+    managing = Managing_committee.objects.all()
+    
+    context = {
+        'managing': managing
+        }
+    return render(request, 'main_app/managing_committee.html',context)
